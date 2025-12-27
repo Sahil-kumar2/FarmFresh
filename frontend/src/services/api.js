569c0fd65ed3efa1c5c8892b7c99c26c666234e5
Sahil Kumar <sahilkumaratwork@gmail.com>
2025-12-27 12:33:53 +0530

    .

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: "https://farmfresh-qzky.onrender.com",
 });
 
 // attach token automatically
